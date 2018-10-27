3f7b81ec0e29c8d30e0dcf5e1cdd9229c46148f1
olgaDorio <olga.mityugova@gmail.com>
2018-10-27 15:37:45 +0300

    add insertionSort

--- sort.js
+++ sort.js
@@ -30,8 +30,27 @@ const selectionSort = (array=[]) => {
   return _array;
 }
 
+const insertionSort = (array=[]) => {
+  const _array = array.slice();
+
+  for (let i = 1; i < _array.length; i++) {
+    let j = i - 1; // indexOf minValue of sorted array
+    let comparedValue = _array[i];
+
+    while (comparedValue < _array[j]) {
+      _array[j + 1] = _array[j]
+      j = j - 1;
+    }
+
+    _array[j + 1] = comparedValue;
+  }
+
+  return _array;
+}
+
 const arr = Array.from({length: 100}, () => Math.round(-500 + Math.random() * 1000));
 
 console.log('initial: ', arr)
 // console.log('bubbleSort: ', bubbleSort(arr));
-console.log('selectionSort: ', selectionSort(arr));
+// console.log('selectionSort: ', selectionSort(arr));
+console.log('insertionSort: ', insertionSort(arr));
